b3aab48ef1495a564b4cf868311f30cc957e416d
서상영 <syseo0509@gmail.com>
2023-06-21 18:06:57 +0900

    remind

---
+++ assets/js/common.js
@@ -0,0 +1,16 @@
+let title = document.querySelector(".hello");
+let element = document.getElementById('helloid');
+document.getElementById("helloid").style.marginTop = "-" + (element.clientHeight) + "px";
+
+function handleClick(){
+    let clickedClass = "clicked";
+    if (title.classList.contains(clickedClass)){
+        title.classList.remove(clickedClass);
+        title.style.marginTop = "-" + (element.clientHeight) + "px";
+        console.log("닫힘");
+    } else {
+        title.classList.add(clickedClass);
+        title.style.marginTop = 10 + "px";
+        console.log(element.clientHeight);
+    };
+}
