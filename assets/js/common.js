7ad39ec60c48e9b90b1cd1f90b78c02d41d05a61
서상영 <syseo0509@gmail.com>
2023-08-08 14:21:09 +0900

    front aside : 230808

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,14 +1,6 @@
-function handleClick(){
-    let title = document.querySelector(".hello");
-    let element = document.getElementById('helloid');
-    let clickedClass = "clicked";
-    if (title.classList.contains(clickedClass)){
-        title.classList.remove(clickedClass);
-        title.style.marginTop = "-" + (element.clientHeight) + "px";
-        console.log("닫힘");
-    } else {
-        title.classList.add(clickedClass);
-        title.style.marginTop = 10 + "px";
-        console.log(element.clientHeight);
-    };
-}
+// Aside Menu 토글
+const menu = document.querySelector(".menu");
+const menuToggle = document.querySelector(".btn-menuToggle");
+menuToggle.addEventListener('click', function(){
+    menu.classList.toggle("active");
+});
