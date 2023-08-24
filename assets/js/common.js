d480034434a11388bb587e7f7e99cab08a5912c4
서상영 <syseo0509@gmail.com>
2023-08-24 12:10:16 +0900

    testseo js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,4 @@
-// Aside Menu 토글 2
+// Aside Menu 토글 22s
 const menu = document.querySelector(".menu");
 const menuToggle = document.querySelector(".btn-menuToggle");
 menuToggle.addEventListener('click', function(){
