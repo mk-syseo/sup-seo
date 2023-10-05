72aca8ed1c56eb20c86e20fe48d1d16fdc037829
서상영 <syseo0509@gmail.com>
2023-10-05 18:47:41 +0900

    Update : front

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,8 +1,10 @@
 // Aside Menu 토글
 const menu = document.querySelector(".menu");
 const menuToggle = document.querySelector(".btn-menuToggle");
+const noscrolls = document.querySelector(".sub");
 menuToggle.addEventListener('click', function(){
     menu.classList.toggle("active");
+    noscrolls.classList.toggle("noscroll");
 });
 
 //높이값 테스트
