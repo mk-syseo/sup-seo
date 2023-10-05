0b215f3417cd9011bbf09d91b3195cae82bc0955
서상영 <syseo0509@gmail.com>
2023-10-05 20:03:34 +0900

    Update : dim effect

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,9 +2,14 @@
 const menu = document.querySelector(".menu");
 const menuToggle = document.querySelector(".btn-menuToggle");
 const noscrolls = document.querySelector(".sub");
+const menuDim = document.querySelector(".dim");
 menuToggle.addEventListener('click', function(){
     menu.classList.toggle("active");
     noscrolls.classList.toggle("noscroll");
+});
+menuDim.addEventListener('click', function(){
+    menu.classList.remove("active");
+    noscrolls.classList.remove("noscroll");
 });
 
 //높이값 테스트
